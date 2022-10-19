e5d8e2c5aea700ec4db3f0d257d2b6b51df9cf87
Michael Chan <michaelcychan@gmail.com>
2022-10-19 21:21:01 +0100

    add tests and a CI workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('opening the webpage', () => {
+  test('title', () => {
+    render(<App />);
+    const header = document.getElementsByClassName('App-header')[0].innerHTML;
+    expect(header).toBe('<h1>Echo Site.</h1>')
+  })
+
+  test('returns the same one word', () => {
+    const page = render(<App />);
+    const input = document.getElementById('shout')
+    fireEvent.change(input, {target: {value: 'this'}})
+    const echo = document.getElementsByClassName('echo')[0].innerHTML
+    expect(echo).toBe('this')
+  })
+
+  test('returns a modified text', () => {
+    const page = render(<App />);
+    const input = document.getElementById('shout')
+    fireEvent.change(input, {target: {value: 'This is an echo cave'}})
+    const echo = document.getElementsByClassName('echo')[0].innerHTML
+    expect(echo).toBe('This is an echo cave... cave... cave...')
+  })
+
+})
