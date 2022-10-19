fdca2730dbaf55a2b9930c1fc8aa95df77dcfdeb
Michael Chan <michaelcychan@gmail.com>
2022-10-19 19:08:07 +0100

    add the echo and update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [echo, setEcho] = useState('')
+  const [shout, setShout] = useState('')
+
+  const changeText = (event: any) => {
+    
+    const text: string = event.target.value
+    setShout(text)
+    const textArray: string[] = text.trim().split(' ')
+    if (textArray.length > 2) {
+      textArray[textArray.length - 1] += '...' 
+      for (let i = 0; i < 2; ++i){
+        textArray.push(textArray[textArray.length - 1])
+      }
+    }
+    setEcho(textArray.join(' '))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Echo Site.
+        </h1>
       </header>
+      <section>
+        <h2>Say Something:</h2>
+        <input
+          type="text"
+          id="shout"
+          name="shout"
+          value={shout}
+          onChange={changeText}
+        />
+      </section>
+      <section className='echo'>
+        {echo}
+      </section>
     </div>
   );
 }
